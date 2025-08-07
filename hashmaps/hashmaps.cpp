2031fdf37a9c77af85fd6b8fb5a3325b014a7ea9
kumar sambhav <sambhav005sharma@gmail.com>
2025-08-07 09:41:07 +0530

    modified

--- hashmaps/hashmaps.cpp
+++ hashmaps/hashmaps.cpp
@@ -1,28 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+// hashing integers
+//  int main(){
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+
+//   int n;
+//   cin >> n;
+//   int arr[n];
+//   for(int i=0;i<n;i++){
+//     cin>> arr[i];
+//   }
+//   //precompute
+//  int hash[101]={0};
+//  for(int i=0;i<n;i++){
+//   hash[arr[i]] += 1;
+//  }
+
+//   int q;
+//   cin >>q;
+//   while(q--){
+//     int number;
+//     cin>>number;
+
+//     cout<<hash[number]<<endl;
+//   }
+// }
+
+// hashing characters
+
 int main(){
-      freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-      int n;
-      cin >> n;
-      int arr[n];
-      for(int i =0;i<n;i++){
-        cin>>arr[i];
-
-      }
-      //precompute
-      int hash[13] = {0};
-      for (int i =0;i<n;i++){
-        hash[arr[i]] += 1;
-      }
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  string s;
+  cin >> s;
+  //precompute
+int hash[256] = {0};
+for(int i=0;i<s.size();i++){
+  hash[s[i]]++;
+}
 
 
-      int q;
-      cin >>q;
-      while (q--){
-        int number;
-        cin>>number;
-        cout<<hash[number]<<endl;
-      }
+  int q;
+  cin>>q;
+  while (q--){
+    char c;
+    cin>>c;
+    cout<<hash[c]<<endl;
+
+    
+  }
 }
