7d882322977e1c373ccdd533f6c9dd45d4dc91fd
kumar sambhav <sambhav005sharma@gmail.com>
2025-08-12 22:24:04 +0530

    ss

---
+++ 5.mergesort/mergersort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int x;
+    cin>> x;
+    int n;
+    cin>> n;
+     for (int i = 0; i < n; i++)
+     {
+    cout<<"sss0";
+     }
+     
+    return 0;
+}
