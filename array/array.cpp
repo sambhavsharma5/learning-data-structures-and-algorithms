6cb5e5cbff49459acd5fc2ff8983213dd15ec1f0
kumar sambhav <sambhav005sharma@gmail.com>
2025-08-15 21:02:56 +0530

    a

---
+++ array/array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void largest(int arr[],int n){
+    int l =arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(l<=arr[i]) l = arr[i];
+    }
+    cout<<"largest element is "<<l;
+}
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    largest(arr,n);
+    
+    return 0;
+}
