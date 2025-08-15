7ff68cb33cbd08c0a7174930db33c139fd80ee78
kumar sambhav <sambhav005sharma@gmail.com>
2025-08-15 07:43:02 +0530

    done

--- 5.mergesort/mergersort.cpp
+++ 5.mergesort/mergersort.cpp
@@ -1,17 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    int x;
-    cin>> x;
-    int n;
-    cin>> n;
-     for (int i = 0; i < n; i++)
-     {
-    cout<<"sss0";
-     }
-     
+void merge(vector<int> &arr, int st, int mid, int end)
+{
+    vector<int> temp;
+    int i = st;
+    int j = mid + 1;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+
+        else
+            temp.push_back(arr[j]);
+        j++;
+    }
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while (j <= end)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        arr[idx + st] = temp[idx];
+    }
+}
+void mergesort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + (end - st) / 2;
+
+        mergesort(arr, st, mid);
+        mergesort(arr, mid + 1, end);
+        merge(arr, st, mid, end);
+    }
+}
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    vector<int> arr = {5, 3, 2, 1};
+    mergesort(arr, 0, arr.size() - 1);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
